3d64bc054fc5d9efb55b5b9a5d8f8772040994cd
hyejin kim <sikhye823@naver.com>
2020-02-18 20:02:34 +0900

    Adding permission on Camera

--- My Application/LineProject/app/src/main/java/com/example/myapplication/activity/MemoWriteActivity.java
+++ My Application/LineProject/app/src/main/java/com/example/myapplication/activity/MemoWriteActivity.java
@@ -1,20 +1,40 @@
 package com.example.myapplication.activity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+import com.squareup.picasso.Picasso;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import com.example.myapplication.R;
-
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MemoWriteActivity extends AppCompatActivity {
@@ -22,15 +42,21 @@ public class MemoWriteActivity extends AppCompatActivity {
     public static final int PICK_FOR_SUB_IMAGE = 2000;
     public static final int NEW_RECIPE = 3000;
     public static final int MODIFY_MY_RECIPE = 4000;
-
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int PICK_FROM_CAMERA = 2;
+    private static final String TAG ="FindMessage";
     List<Uri> fileUris = new ArrayList<>();
     private EditText mWriteContent;
     private Intent intent;
+    private ImageView mImage;
+    private File tempFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_memo);
 
+//        tedPermission();
 //        Uri dummyForCheck = null;
 //        fileUris.add(dummyForCheck);
         Toolbar toolbar = findViewById(R.id.toolbar_memo_form);
@@ -70,5 +96,104 @@ public class MemoWriteActivity extends AppCompatActivity {
 //                }
             }
         });
+        mImage = findViewById(R.id.memo_image);
+        mImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 사진 요청
+//                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(intent, PICK_FOR_MAIN_IMAGE);
+                goToAlbum();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(this, "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            if(tempFile != null) {
+                if (tempFile.exists()) {
+                    if (tempFile.delete()) {
+                        Log.e(TAG, tempFile.getAbsolutePath() + " 삭제 성공");
+                        tempFile = null;
+                    }
+                }
+            }
+            return;
+        }
+        if (requestCode == PICK_FROM_ALBUM && intent!=null) {
+            Uri photoUri = data.getData();
+            if(photoUri!=null){
+                setImage();
+            }
+        }
+        else if (requestCode == PICK_FROM_CAMERA) {
+
+            setImage();
+
+        }
+    }
+    private File createImageFile() throws IOException {
+
+        // 이미지 파일 이름 ( blackJin_{시간}_ )
+        String timeStamp = new SimpleDateFormat("HHmmss").format(new Date());
+        String imageFileName = "blackJin_" + timeStamp + "_";
+
+        // 이미지가 저장될 폴더 이름 ( blackJin )
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/blackJin/");
+        if (!storageDir.exists()) storageDir.mkdirs();
+
+        // 빈 파일 생성
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        return image;
+    }
+
+
+
+    private void setImage() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
+        mImage.setImageBitmap(originalBm);
+    }
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            tempFile = createImageFile();
+        } catch (IOException e) {
+            Toast.makeText(this, "이미지 처리 오류! 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+            finish();
+            e.printStackTrace();
+        }
+        if (tempFile != null) {
+            Uri photoUri = Uri.fromFile(tempFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(intent, PICK_FROM_CAMERA);
+        }
+    }
+
+//    private void tedPermission() {
+//        PermissionListener permissionListener = new PermissionListener() {
+//            @Override
+//            public void onPermissionGranted() {
+//                // 권한 요청 성공
+//            }
+//            @Override
+//            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+//                // 권한 요청 실패
+//            }
+//        };
+//        TedPermission.with(this)
+//                .setPermissionListener(permissionListener)
+//                .setRationaleMessage(getResources().getString(R.string.permission_2))
+//                .setDeniedMessage(getResources().getString(R.string.permission_1))
+//                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+//                .check();
+//    }
+    private void goToAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
     }
 }
